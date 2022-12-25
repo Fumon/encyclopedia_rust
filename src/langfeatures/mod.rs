70b9225faeaa770aabaa55753fc896431e5bf310
Jade Bilkey <425547+Fumon@users.noreply.github.com>
2022-12-25 15:44:24 -0500

    add:no compound comparisons

--- src/langfeatures/mod.rs
+++ src/langfeatures/mod.rs
@@ -1,3 +1,4 @@
 mod flatten;
 mod none_range;
 mod inner_func;
+mod no_compound_comparisons;
---
+++ src/langfeatures/no_compound_comparisons.rs
@@ -0,0 +1,8 @@
+
+#[test]
+fn m() {
+    // Doesn't work
+    // if 5 < 6 < 7 {
+    //     println("yeah");
+    // }
+}
